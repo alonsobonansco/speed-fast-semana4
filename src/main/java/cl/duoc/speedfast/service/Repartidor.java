fa3d9372e5914a7a089ca22de525656a2ed8e309
Alonso Bonansco <al.bonansco@duocuc.cl>
2026-09-05 16:08:09 -0400

    feat: implementar interfaz Rastreable en clase Repartidor

--- src/main/java/cl/duoc/speedfast/service/Repartidor.java
+++ src/main/java/cl/duoc/speedfast/service/Repartidor.java
@@ -1,14 +1,17 @@
 package cl.duoc.speedfast.service;
 
+import cl.duoc.speedfast.interfaces.Rastreable;
 import cl.duoc.speedfast.model.Pedido;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Repartidor implements Runnable {
+public class Repartidor implements Runnable, Rastreable {
     private final String nombreRepartidor;
     private final List<Pedido> pedidosOrdenados;
+    private final static List<Pedido> HISTORIAL_PEDIDOS = new CopyOnWriteArrayList<>();
 
     public Repartidor(String nombreRepartidor, List<Pedido> pedidosIniciales) {
         this.nombreRepartidor = nombreRepartidor;
@@ -24,17 +27,49 @@ public class Repartidor implements Runnable {
     public void run() {
         for (Pedido pedido : pedidosOrdenados) {
             try {
-                System.out.println("[Repartidor: " + nombreRepartidor + "] Entregando " +
-                        pedido.getTipoPedido().getNombrePedido() + " #" + pedido.getIdPedido() + "...");
+                if (pedido.isPedidoActivo()) {
+                    System.out.println("[Repartidor: " + nombreRepartidor + "] Entregando " +
+                            pedido.getTipoPedido().getNombrePedido() + " #" + pedido.getIdPedido() + "...");
 
-                int tiempoViaje = 1000 + ThreadLocalRandom.current().nextInt(1500);
-                Thread.sleep(tiempoViaje);
+                    int tiempoViaje = 3000 + ThreadLocalRandom.current().nextInt(1500);
+                    Thread.sleep(tiempoViaje);
 
-                System.out.println("[Repartidor: " + nombreRepartidor + "] Pedido #" + pedido.getIdPedido() +
-                        " entregado.");
+                    System.out.println("[Repartidor: " + nombreRepartidor + "] Pedido #" + pedido.getIdPedido() +
+                            " entregado ✔");
+                }
+
+                registrarPedido(pedido);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 return;
+            }
+        }
+    }
+
+    public void registrarPedido(Pedido pedido) {
+        if (pedido != null) {
+            HISTORIAL_PEDIDOS.add(pedido);
+        }
+    }
+
+    @Override
+    public void verHistorial() {
+        if (HISTORIAL_PEDIDOS.isEmpty()) {
+            System.out.println("No se han registrado pedidos en esta sesión.");
+            return;
+        }
+
+        String tituloHistorial = """
+                \n======================================
+                === HISTORIAL DE TODAS LAS ÓRDENES ===
+                ======================================\n""";
+        System.out.println(tituloHistorial);
+
+        for (Pedido pedido : HISTORIAL_PEDIDOS) {
+            if (pedido.isPedidoActivo()) {
+                System.out.println("- " + pedido.getTipoPedido().getNombrePedido() + " #" + pedido.getIdPedido() + " - [Entregado exitósamente]");
+            } else {
+                System.out.println("- " + pedido.getTipoPedido().getNombrePedido() + " #" + pedido.getIdPedido() + " - [Orden cancelada]");
             }
         }
     }
